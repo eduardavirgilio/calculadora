ec2c2deb03c381d4e49ef1b50a5e133362197793
Allicia Morete <allicia.morete4300@gmail.com>
2024-10-01 08:36:14 -0300

    add: menu strip

--- fmrCalculadora.Designer.cs
+++ fmrCalculadora.Designer.cs
@@ -47,25 +47,31 @@
             btn0 = new Button();
             btnCalcular = new Button();
             pictureBox1 = new PictureBox();
+            menuStrip1 = new MenuStrip();
+            conversorToolStripMenuItem = new ToolStripMenuItem();
+            celsiusParaFahrenheitToolStripMenuItem = new ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // txtAparecer
             // 
             txtAparecer.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            txtAparecer.Location = new Point(12, 25);
+            txtAparecer.Location = new Point(25, 49);
+            txtAparecer.Margin = new Padding(3, 4, 3, 4);
             txtAparecer.Multiline = true;
             txtAparecer.Name = "txtAparecer";
-            txtAparecer.Size = new Size(285, 61);
+            txtAparecer.Size = new Size(339, 80);
             txtAparecer.TabIndex = 0;
             // 
             // btnSomar
             // 
             btnSomar.BackColor = Color.Thistle;
             btnSomar.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnSomar.Location = new Point(12, 106);
+            btnSomar.Location = new Point(25, 157);
+            btnSomar.Margin = new Padding(3, 4, 3, 4);
             btnSomar.Name = "btnSomar";
-            btnSomar.Size = new Size(59, 56);
+            btnSomar.Size = new Size(67, 75);
             btnSomar.TabIndex = 1;
             btnSomar.Text = "+";
             btnSomar.UseVisualStyleBackColor = false;
@@ -75,9 +81,10 @@
             // 
             btnSubtrair.BackColor = Color.Thistle;
             btnSubtrair.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnSubtrair.Location = new Point(86, 106);
+            btnSubtrair.Location = new Point(109, 157);
+            btnSubtrair.Margin = new Padding(3, 4, 3, 4);
             btnSubtrair.Name = "btnSubtrair";
-            btnSubtrair.Size = new Size(59, 56);
+            btnSubtrair.Size = new Size(67, 75);
             btnSubtrair.TabIndex = 2;
             btnSubtrair.Text = "-";
             btnSubtrair.UseVisualStyleBackColor = false;
@@ -87,9 +94,10 @@
             // 
             btnMultiplicar.BackColor = Color.Thistle;
             btnMultiplicar.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnMultiplicar.Location = new Point(160, 106);
+            btnMultiplicar.Location = new Point(194, 157);
+            btnMultiplicar.Margin = new Padding(3, 4, 3, 4);
             btnMultiplicar.Name = "btnMultiplicar";
-            btnMultiplicar.Size = new Size(59, 56);
+            btnMultiplicar.Size = new Size(67, 75);
             btnMultiplicar.TabIndex = 3;
             btnMultiplicar.Text = "*";
             btnMultiplicar.UseVisualStyleBackColor = false;
@@ -99,9 +107,10 @@
             // 
             btnDividir.BackColor = Color.Thistle;
             btnDividir.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnDividir.Location = new Point(238, 106);
+            btnDividir.Location = new Point(283, 157);
+            btnDividir.Margin = new Padding(3, 4, 3, 4);
             btnDividir.Name = "btnDividir";
-            btnDividir.Size = new Size(59, 56);
+            btnDividir.Size = new Size(81, 75);
             btnDividir.TabIndex = 4;
             btnDividir.Text = "/";
             btnDividir.UseVisualStyleBackColor = false;
@@ -111,9 +120,10 @@
             // 
             btn01.BackColor = Color.Thistle;
             btn01.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn01.Location = new Point(12, 179);
+            btn01.Location = new Point(25, 255);
+            btn01.Margin = new Padding(3, 4, 3, 4);
             btn01.Name = "btn01";
-            btn01.Size = new Size(59, 56);
+            btn01.Size = new Size(67, 75);
             btn01.TabIndex = 5;
             btn01.Text = "1";
             btn01.UseVisualStyleBackColor = false;
@@ -123,9 +133,10 @@
             // 
             btn02.BackColor = Color.Thistle;
             btn02.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn02.Location = new Point(86, 179);
+            btn02.Location = new Point(109, 255);
+            btn02.Margin = new Padding(3, 4, 3, 4);
             btn02.Name = "btn02";
-            btn02.Size = new Size(59, 56);
+            btn02.Size = new Size(67, 75);
             btn02.TabIndex = 6;
             btn02.Text = "2";
             btn02.UseVisualStyleBackColor = false;
@@ -135,9 +146,10 @@
             // 
             btn03.BackColor = Color.Thistle;
             btn03.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn03.Location = new Point(160, 179);
+            btn03.Location = new Point(194, 255);
+            btn03.Margin = new Padding(3, 4, 3, 4);
             btn03.Name = "btn03";
-            btn03.Size = new Size(59, 56);
+            btn03.Size = new Size(67, 75);
             btn03.TabIndex = 7;
             btn03.Text = "3";
             btn03.UseVisualStyleBackColor = false;
@@ -147,9 +159,10 @@
             // 
             btnLimpar.BackColor = Color.Thistle;
             btnLimpar.Font = new Font("Tahoma", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnLimpar.Location = new Point(238, 179);
+            btnLimpar.Location = new Point(283, 255);
+            btnLimpar.Margin = new Padding(3, 4, 3, 4);
             btnLimpar.Name = "btnLimpar";
-            btnLimpar.Size = new Size(59, 56);
+            btnLimpar.Size = new Size(81, 75);
             btnLimpar.TabIndex = 8;
             btnLimpar.Text = "CLS";
             btnLimpar.UseVisualStyleBackColor = false;
@@ -159,9 +172,10 @@
             // 
             btn04.BackColor = Color.Thistle;
             btn04.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn04.Location = new Point(12, 250);
+            btn04.Location = new Point(25, 349);
+            btn04.Margin = new Padding(3, 4, 3, 4);
             btn04.Name = "btn04";
-            btn04.Size = new Size(59, 56);
+            btn04.Size = new Size(67, 75);
             btn04.TabIndex = 9;
             btn04.Text = "4";
             btn04.UseVisualStyleBackColor = false;
@@ -171,9 +185,10 @@
             // 
             btn05.BackColor = Color.Thistle;
             btn05.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn05.Location = new Point(86, 250);
+            btn05.Location = new Point(109, 349);
+            btn05.Margin = new Padding(3, 4, 3, 4);
             btn05.Name = "btn05";
-            btn05.Size = new Size(59, 56);
+            btn05.Size = new Size(67, 75);
             btn05.TabIndex = 10;
             btn05.Text = "5";
             btn05.UseVisualStyleBackColor = false;
@@ -183,9 +198,10 @@
             // 
             btn06.BackColor = Color.Thistle;
             btn06.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn06.Location = new Point(160, 250);
+            btn06.Location = new Point(194, 349);
+            btn06.Margin = new Padding(3, 4, 3, 4);
             btn06.Name = "btn06";
-            btn06.Size = new Size(59, 56);
+            btn06.Size = new Size(67, 75);
             btn06.TabIndex = 11;
             btn06.Text = "6";
             btn06.UseVisualStyleBackColor = false;
@@ -195,9 +211,10 @@
             // 
             btn07.BackColor = Color.Thistle;
             btn07.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn07.Location = new Point(12, 321);
+            btn07.Location = new Point(25, 444);
+            btn07.Margin = new Padding(3, 4, 3, 4);
             btn07.Name = "btn07";
-            btn07.Size = new Size(59, 56);
+            btn07.Size = new Size(67, 75);
             btn07.TabIndex = 12;
             btn07.Text = "7";
             btn07.UseVisualStyleBackColor = false;
@@ -207,9 +224,10 @@
             // 
             btn08.BackColor = Color.Thistle;
             btn08.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn08.Location = new Point(86, 321);
+            btn08.Location = new Point(109, 444);
+            btn08.Margin = new Padding(3, 4, 3, 4);
             btn08.Name = "btn08";
-            btn08.Size = new Size(59, 56);
+            btn08.Size = new Size(67, 75);
             btn08.TabIndex = 13;
             btn08.Text = "8";
             btn08.UseVisualStyleBackColor = false;
@@ -219,9 +237,10 @@
             // 
             btn09.BackColor = Color.Thistle;
             btn09.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn09.Location = new Point(160, 321);
+            btn09.Location = new Point(194, 444);
+            btn09.Margin = new Padding(3, 4, 3, 4);
             btn09.Name = "btn09";
-            btn09.Size = new Size(59, 56);
+            btn09.Size = new Size(67, 75);
             btn09.TabIndex = 14;
             btn09.Text = "9";
             btn09.UseVisualStyleBackColor = false;
@@ -231,9 +250,10 @@
             // 
             btn0.BackColor = Color.Thistle;
             btn0.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn0.Location = new Point(12, 393);
+            btn0.Location = new Point(25, 540);
+            btn0.Margin = new Padding(3, 4, 3, 4);
             btn0.Name = "btn0";
-            btn0.Size = new Size(207, 56);
+            btn0.Size = new Size(237, 75);
             btn0.TabIndex = 15;
             btn0.Text = "0";
             btn0.UseVisualStyleBackColor = false;
@@ -243,9 +263,10 @@
             // 
             btnCalcular.BackColor = Color.Thistle;
             btnCalcular.Font = new Font("Tahoma", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnCalcular.Location = new Point(238, 250);
+            btnCalcular.Location = new Point(283, 349);
+            btnCalcular.Margin = new Padding(3, 4, 3, 4);
             btnCalcular.Name = "btnCalcular";
-            btnCalcular.Size = new Size(59, 199);
+            btnCalcular.Size = new Size(81, 265);
             btnCalcular.TabIndex = 16;
             btnCalcular.Text = "=";
             btnCalcular.UseVisualStyleBackColor = false;
@@ -255,19 +276,43 @@
             // 
             pictureBox1.BackColor = Color.Transparent;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(56, 455);
+            pictureBox1.Location = new Point(79, 622);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(194, 122);
+            pictureBox1.Size = new Size(222, 163);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 17;
             pictureBox1.TabStop = false;
             // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { conversorToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(393, 28);
+            menuStrip1.TabIndex = 18;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // conversorToolStripMenuItem
+            // 
+            conversorToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { celsiusParaFahrenheitToolStripMenuItem });
+            conversorToolStripMenuItem.Name = "conversorToolStripMenuItem";
+            conversorToolStripMenuItem.Size = new Size(89, 24);
+            conversorToolStripMenuItem.Text = "Conversor";
+            // 
+            // celsiusParaFahrenheitToolStripMenuItem
+            // 
+            celsiusParaFahrenheitToolStripMenuItem.Name = "celsiusParaFahrenheitToolStripMenuItem";
+            celsiusParaFahrenheitToolStripMenuItem.Size = new Size(243, 26);
+            celsiusParaFahrenheitToolStripMenuItem.Text = "Celsius para Fahrenheit";
+            // 
             // fmrCalculadora
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.GhostWhite;
-            ClientSize = new Size(319, 571);
+            ClientSize = new Size(393, 782);
             Controls.Add(pictureBox1);
             Controls.Add(btnCalcular);
             Controls.Add(btn0);
@@ -286,11 +331,16 @@
             Controls.Add(btnSubtrair);
             Controls.Add(btnSomar);
             Controls.Add(txtAparecer);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(3, 4, 3, 4);
             Name = "fmrCalculadora";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "fmrCalculadora";
             Load += fmrCalculadora_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -331,5 +381,8 @@
         private Button btn09;
         private Button btn0;
         private Button btnCalcular;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem conversorToolStripMenuItem;
+        private ToolStripMenuItem celsiusParaFahrenheitToolStripMenuItem;
     }
 }
